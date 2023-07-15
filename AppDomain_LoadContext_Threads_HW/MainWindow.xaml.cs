46171ede56baa4ead7e2df67efd07ba888b59808
Standard Computer's <Standard Computer's@DESKTOP-FRC77TS>
2023-07-15 12:35:56 +0300

    son

--- AppDomain_LoadContext_Threads_HW/MainWindow.xaml.cs
+++ AppDomain_LoadContext_Threads_HW/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,93 @@ namespace AppDomain_LoadContext_Threads_HW
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Thread Thread { get; set; }
+     
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void CopyFile(string from, string to)
+        {
+            if (File.Exists(from))
+            {
+                Thread thread = new Thread(() =>
+                {
+                    using (FileStream fsread = new FileStream(from, FileMode.Open, FileAccess.Read))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            ProgressBar.Maximum = fsread.Length;
+                        });
+
+                        using (FileStream fswrite = new FileStream(to, FileMode.Create, FileAccess.Write))
+                        {
+                            int len = 1;
+                            var filesize = fsread.Length;
+                            byte[] buffer = new byte[len];
+                            int size = 0;
+                            do
+                            {
+                                len = fsread.Read(buffer, 0, buffer.Length);
+                                fswrite.Write(buffer, 0, len);
+                                filesize -= len;
+                                size += len;
+                                Dispatcher.Invoke(() =>
+                                {
+                                    ProgressBar.Value = size;
+                                });
+                                Thread.Sleep(1);
+                                fswrite.Flush();
+                            } while (len != 0);
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                MessageBox.Show("Copy Succesful");
+                                ProgressBar.Value = 0;
+                                Thread = null;
+                                TBFrom.Text = default;
+                                TBTo.Text = default;
+                                
+                            });
+                        }
+                    }
+                });
+                Thread = thread;
+                Thread.IsBackground = true;
+                Thread.Start();
+            }
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (Thread == null)
+            {
+                CopyFile(TBFrom.Text, TBTo.Text);
+            }
+        }
+        [Obsolete]
+
+        private void FromOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "(*.txt)|*.txt";
+            fileDialog.ShowDialog();
+            if (fileDialog.FileName != "" && Thread == null)
+            {
+                TBFrom.Text = fileDialog.FileName;
+            }
+        }
+
+        private void ToOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "(*.txt)|*.txt";
+            file.ShowDialog();
+            if (file.FileName != null && Thread == null && file.FileName != TBFrom.Text)
+            {
+                TBTo.Text = file.FileName;
+            }
         }
     }
 }
